c1e3fa8ca634d05e85d8a3334770f6811f40c1a3
KaZiUpl <38405516+KaZiUpl@users.noreply.github.com>
2018-05-23 21:52:26 +0200

    multithreaded prime checking

---
+++ is it prime.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+#define maksymalna pow(10,3)
+int licznik;
+
+int czy_pierwsza()
+{
+    int prime_test = licznik;
+    licznik++;
+
+    int i,flaga=1;
+    for(i=2 ; i*i<=prime_test ; ++i)
+    {
+        if(prime_test%i == 0 && prime_test != 2)
+        {
+            flaga = 0;
+            break;
+        }
+    }
+    if(flaga == 1) printf("%d\n",prime_test);
+}
+
+int main(void)
+{
+    int i;
+    licznik = 2;
+    while(licznik < maksymalna)
+    {
+        czy_pierwsza();
+    }
+    return 0;
+}
--- is it prime_multithread.c
+++ is it prime_multithread.c
@@ -1,40 +1,5 @@
 #include<stdio.h>
 #include<math.h>
-
-#define maksymalna pow(10,9)
-int licznik;
-
-int czy_pierwsza()
-{
-    int prime_test = licznik;
-    licznik++;    
-    int i,flaga=1;
-    for(i=2 ; i<=sqrt(prime_test) ; ++i)
-    {
-        if(prime_test%i == 0 && prime_test != 2) 
-        {           
-            flaga = 0;
-            break;
-        }
-    }
-    if(flaga == 1) printf("%d\n",i);
-}
-
-int main(void)
-{
-    int i;
-    licznik = 2;
-    while(licznik < maksymalna)
-    {
-        czy_pierwsza();   
-    }
-    return 0;
-}
-
-
-
-#include<stdio.h>
-#include<math.h>
 #include<pthread.h>
 
 #define maksymalna pow(10,2)
@@ -48,10 +13,10 @@ void* czy_pierwsza(void *arg)
 {
     int numer_watku = (int) arg;
     int prime_test;
-    int i,flaga;   
+    int i,flaga;
     do
     {
-        flaga = 1;        
+        flaga = 1;
         pthread_mutex_lock(&mutex);
         prime_test = licznik;
         licznik++;
@@ -59,16 +24,17 @@ void* czy_pierwsza(void *arg)
 
         for(i=2 ; i*i<=prime_test ; ++i)
         {
-            if(prime_test%i == 0 && prime_test != 2) 
-            {           
+            if(prime_test%i == 0 && prime_test != 2)
+            {
                 flaga = 0;
                 break;
             }
         }
-        if(flaga == 1) printf("%d {watek: %d}\n ",i,numer_watku);
+        if(flaga == 1) printf("%d {watek: %d}\n ",prime_test,numer_watku);
+        /*if(flaga == 1) printf("%d {watek: %d}\n ",prime_test,numer_watku);*/
 
     }while(prime_test <= maksymalna);
-    
+
 
     return NULL;
 }
@@ -81,7 +47,7 @@ int main(void)
 
     /* Inicjalizacja blokady */
 	pthread_mutex_init(&mutex,NULL);
-    
+
     /* Stworzenie i uruchomienie watkow */
 	for(i=0;i<ilosc_watkow;++i){
 		if(pthread_create(&thread[i],NULL,czy_pierwsza,(void*)i) != 0){
@@ -94,9 +60,9 @@ int main(void)
 	for(i=0;i<ilosc_watkow;++i){
 		pthread_join(thread[i],NULL);
 	}
-	
+
 	/* Usuniecie blokady */
-	pthread_mutex_destroy(&mutex);    
+	pthread_mutex_destroy(&mutex);
 
     return 0;
 }
