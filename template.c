32acb047ef78c23b469df9ed64adac7b74569384
KaZiUpl <38405516+KaZiUpl@users.noreply.github.com>
2018-05-26 23:29:53 +0200

    tidying stuff

---
+++ template.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define FOR(i,a,b) for(i=a;i<b;++i)
+#define FORd(i,a,b) for(i=a;i>b;--i)
+#define FOReq(i,a,b) for(i=a;i<=b;++i)
+#define FORdeq(i,a,b) for(i=a;i>=b;--i)
+#define print printf
+#define scan scanf
+
+int main(void)
+{
+
+    return 0;
+}
