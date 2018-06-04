7af47a14a21ec00f6ddebcd5956106a4b2b85d17
KaZiUpl <38405516+KaZiUpl@users.noreply.github.com>
2018-06-04 21:46:49 +0200

    Grazing Cow

---
+++ 10678 - The Grazing Cow.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+#include<math.h>
+
+#define FOR(i,a,b) for(i=a;i<b;++i)
+#define FORd(i,a,b) for(i=a;i>b;--i)
+#define FOReq(i,a,b) for(i=a;i<=b;++i)
+#define FORdeq(i,a,b) for(i=a;i>=b;--i)
+#define print printf
+#define scan scanf
+
+int main(void)
+{
+    int test_cases,i;
+    double odleglosc,dlugosc_liny;
+    double Pi = 2*acos(0), wynik;
+
+    scan("%d",&test_cases);
+
+    FOR(i,0,test_cases)
+    {
+        scan("%lf %lf",&odleglosc,&dlugosc_liny);
+
+        wynik = Pi * (odleglosc/2 + (dlugosc_liny-odleglosc)/2) * sqrt( pow(dlugosc_liny/2,2) - pow(odleglosc/2,2) );
+
+        print("%.3f\n",wynik);
+    }
+
+    return 0;
+}
